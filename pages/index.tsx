1dfd3fb09968d60d619a7769913050e223d42e0b
kodais <82872034+KodaiShinada@users.noreply.github.com>
2021-12-13 19:49:04 +0900

    added bombs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
 import styled from 'styled-components'
+const FONT_COLORS = ['blue', 'green', 'red', 'purple', 'brown', 'yellow', 'orange', 'pink']
 
 const Container = styled.div`
   height: 100vh;
@@ -14,10 +15,10 @@ const Board = styled.div`
   display: row;
 `
 
-const Board_up = styled.div`
+const BoardUp = styled.div`
   height: 19vh;
   width: 81vh;
-  background: gray;
+  background: blue;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -30,21 +31,43 @@ const Face = styled.div`
   border-radius: 50%;
 `
 
-const Board_under = styled.div`
+const BoardUnder = styled.div`
   height: 81vh;
   width: 81vh;
   background: red;
 `
 
-const Box = styled.div`
+const Box = styled.div<{ isOpen: boolean; numColor: number }>`
   height: 9vh;
   width: 9vh;
+  background: ${(props) => (props.isOpen ? 'white' : 'gray')};
+  color: ${(props) =>
+    0 < props.numColor && props.numColor <= 9 ? FONT_COLORS[props.numColor - 1] : 'black'};
+  border: 1px solid;
+  border-color: black;
+  border-left: transparent;
+  display: inline-block;
+  vertical-align: bottom;
+  text-align: center;
+  line-height: 8.5vh;
+  font-size: 30px;
+  font-weight: bold;
+`
+
+const BombBlock = styled.div`
+  height: 9vh;
+  width: 9vh;
+  color: red;
   background: white;
   border: 1px solid;
   border-color: black;
   border-left: transparent;
   display: inline-block;
   vertical-align: bottom;
+  text-align: center;
+  line-height: 8.5vh;
+  font-size: 30px;
+  font-weight: bold;
 `
 
 const Home: NextPage = () => {
@@ -60,26 +83,44 @@ const Home: NextPage = () => {
     [9, 9, 9, 9, 9, 9, 9, 9, 9],
     [9, 9, 9, 9, 9, 9, 9, 9, 9]
   ])
+  const [bombs, setBombs] = useState([{ x: 0, y: 0 }])
   const onClick = (x: number, y: number) => {
     const newBoard: number[][] = JSON.parse(JSON.stringify(board))
-    newBoard[y][x] = 1
+    //newBoard[y][x] = 1
+    let existBomb = false
+    for (let i = 0; i < bombs.length; i++) {
+      if (bombs[i].x === x && bombs[i].y === y) {
+        existBomb = true
+      }
+    }
+    newBoard[y][x] = existBomb ? 10 : 1
     setBoard(newBoard)
   }
+
   return (
     <Container>
       <Board>
-        <Board_up>
+        <BoardUp>
           <Face></Face>
-        </Board_up>
-        <Board_under>
+        </BoardUp>
+        <BoardUnder>
           {board.map((row, y) =>
-            row.map((num, x) => (
-              <Box key={`${x}-${y}`} onClick={() => onClick(x, y)}>
-                {num < 9 && num}
-              </Box>
-            ))
+            row.map((num, x) =>
+              num === 10 ? (
+                <BombBlock key={`${x}-${y}`}>●</BombBlock>
+              ) : (
+                <Box
+                  key={`${x}-${y}`}
+                  isOpen={num < 9}
+                  numColor={num}
+                  onClick={() => onClick(x, y)}
+                >
+                  {0 < num && num < 9 && num}
+                </Box>
+              )
+            )
           )}
-        </Board_under>
+        </BoardUnder>
       </Board>
     </Container>
   )
